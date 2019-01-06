d7d6d069a4e092f074b8ce1aa91eaf6233a5d50b
chillemon <46194375+chillemon@users.noreply.github.com>
2019-01-06 18:41:04 +0900

    Update ViewController.swift
    更新

--- learn2/learn2/ViewController.swift
+++ learn2/learn2/ViewController.swift
@@ -10,26 +10,30 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
+    weak var delegate: CalculationDelegate?
+    
     var button: UIButton!
     var resultLabel = UILabel()
-    var xtextField: UITextField!
-    var ytextField: UITextField!
     
+    var xTextField: UITextField!
+    var yTextField: UITextField!
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // スクリーンの横縦幅
         let screenWidth:CGFloat = self.view.frame.width
         let screenHeight:CGFloat = self.view.frame.height
-        
+
         let button = UIButton()
         button.backgroundColor = .red
         button.setTitle("押してくれ", for: .normal)
         button.frame = CGRect(x: screenWidth/4, y: screenHeight/1.5, width: screenWidth/2, height: 50)
-        
+        //ボタンを押した時のイベント
+        button.addTarget(self, action: #selector(buttonDidTapped(_:)), for: UIControl.Event.touchUpInside)
+
         self.view.addSubview(button)
-        
+
         // ラベルのサイズを設定
         resultLabel.frame = CGRect(x:screenWidth/4, y:200,
                              width:screenWidth/2, height:50);
@@ -39,17 +43,17 @@ class ViewController: UIViewController, UITextFieldDelegate {
         resultLabel.textAlignment = NSTextAlignment.center
         // ラベルのフォントサイズ
         resultLabel.font = UIFont.systemFont(ofSize: 36)
-        
+
         resultLabel.backgroundColor = .blue
         // Viewにラベルを追加
         self.view.addSubview(resultLabel)
-        
+
         let xTextField = UITextField()
         xTextField.frame = CGRect(x: screenWidth/1.5, y: screenHeight/2, width: screenWidth/4, height: 50)
         xTextField.backgroundColor = .gray
         xTextField.keyboardType = UIKeyboardType.numberPad
         self.view.addSubview(xTextField)
-        
+
         let yTextField = UITextField()
         yTextField.frame = CGRect(x: screenWidth/8, y: screenHeight/2, width: screenWidth/4, height: 50)
         yTextField.backgroundColor = .cyan
@@ -58,28 +62,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     }
     
+    @objc func buttonDidTapped(_ sender: UIButton) {
+        delegate?.plusButtonDidTapped()
+            
+        }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         self.view.endEditing(true)
     }
-    
-    
-    @objc func buttonTapped(_ sender: UIButton) {
-        
-        let IntX = Int(xtextField.text!)
-        let IntY = Int(ytextField.text!)
-        resultLabel.text = calc(IntX!, IntY!)
-    }
-    
-    func calc(_ xTextField: Int, _ yTextField: Int) -> String {
-        let x = xtextField.text
-        let y = ytextField.text
-        
-        let result = Int(x! + y!)
-        
-        return result!.description
-        
-    }
-    
 
 }
+
+
